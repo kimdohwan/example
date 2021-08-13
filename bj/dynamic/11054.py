14821f4627ff0fcc84c801feaa0c924564d77c99
kimdohwan <39307213+kimdohwan@users.noreply.github.com>
2021-08-13 17:42:41 +0900

    Update 11054.py

--- bj/dynamic/11054.py
+++ bj/dynamic/11054.py
@@ -1,4 +1,32 @@
-n = int(input())
-a = list(map(int, input().split()))
+# n = 10
+# li = [1, 5, 2, 1, 4, 3, 4, 5, 2, 1]
 
-# https://seohyun0120.tistory.com/entry/%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4LIS-%EC%99%84%EC%A0%84-%EC%A0%95%EB%B3%B5-%EB%B0%B1%EC%A4%80-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+n = int(input())
+li = list(map(int, input().split()))
+
+"""
+10
+1 5 2 1 4 3 4 5 2 1
+1 2 2 1 3 3 4 5 2 1
+1 5 2 1 4 3 3 3 2 1
+"""
+
+dp_1 = [0 for _ in range(n)]
+dp_2 = [0 for _ in range(n)]
+for i in range(n):
+    max_x = 0
+    for j in range(i):
+        if li[j] < li[i]:
+            max_x = max(max_x, dp_1[j])
+    dp_1[i] = max_x + 1
+    
+    max_y = 0
+    for k in range(n - 1, n - 1 - i, -1):
+        if li[k] < li[n - 1 - i]:
+            max_y = max(max_y, dp_2[k])
+    dp_2[n - 1 - i] = max_y + 1
+
+dp = [x + y - 1 for x, y in zip(dp_1, dp_2)]
+print(max(dp))
+            
+
